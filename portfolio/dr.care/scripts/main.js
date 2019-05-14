12194721165387902f2a1ee5aded5c43da0fa8eb
Fedor Pasynkov <proggraming@mail.ru>
2019-05-14 06:51:15 +0300

    +lines

--- portfolio/dr.care/scripts/main.js
+++
@@ -1,9 +0,0 @@
-$('.owl-carousel').owlCarousel({
-    autoplay:true,
-    loop:true,
-    items:1,
-    margin: 20,
-    animateIn: 'fadeIn',
-    animateOut: 'fadeOut',
-    navText: ["<span class='ion-md-arrow-back'></span>", "<span class='ion-chevron-right'></span>"],
-})
