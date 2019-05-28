9b8888cc1c40182742ef21918579b78b8a5baf0c
Fedor Pasynkov <proggraming@mail.ru>
2019-05-28 07:02:54 +0300

    +comments -classes +snipps +name-for-snipps +fix-bootstrap-connection +prefix-to-some-snipps

--- portfolio/tests/gulp/js/main.js
+++ portfolio/tests/gulp/js/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
     AOS.init({
         // Global settings:
@@ -21,50 +22,5 @@ $(document).ready(function () {
         anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
     });
-    var siteCarousel = function () {
-        if ($('.nonloop-block-13').length > 0) {
-            $('.nonloop-block-13').owlCarousel({
-                center: false,
-                items: 1,
-                loop: true,
-                stagePadding: 0,
-                margin: 0,
-                autoplay: true,
-                nav: true,
-                navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
-                responsive: {
-                    600: {
-                        margin: 0,
-                        nav: true,
-                        items: 2
-                    },
-                    1000: {
-                        margin: 0,
-                        stagePadding: 0,
-                        nav: true,
-                        items: 3
-                    },
-                    1200: {
-                        margin: 0,
-                        stagePadding: 0,
-                        nav: true,
-                        items: 4
-                    }
-                }
-            });
-        }
-
-        $('.slide-one-item').owlCarousel({
-            center: false,
-            items: 1,
-            loop: true,
-            stagePadding: 0,
-            margin: 0,
-            autoplay: true,
-            pauseOnHover: false,
-            nav: true,
-            navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
-        });
-    };
-    siteCarousel();
+    
 });
