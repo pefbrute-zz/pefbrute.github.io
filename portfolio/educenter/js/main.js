6fee5626f4ec115be7a841ec546ef6248ea96427
Fedor Pasynkov <proggraming@mail.ru>
2019-06-16 07:04:27 +0300

    +preparation

---
+++ portfolio/educenter/js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    /*AOS START*/
+    AOS.init({
+        // Global settings:
+        disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+        initClassName: 'aos-init', // class applied after initialization
+        animatedClassName: 'aos-animate', // class applied on animation
+        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+        offset: 120, // offset (in px) from the original trigger point
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 400, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: false, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+    });
+    /*AOS END*/
+
+    /* Intro-Carousel Start*/
+    var introCarousel = $(".carousel");
+    var introCarouselIndicators = $(".carousel-indicators");
+    introCarousel.find(".carousel-inner").children(".carousel-item").each(function (index) {
+        (index === 0) ?
+        introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "' class='active'></li>"):
+            introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "'></li>");
+
+        $(this).css("background-image", "url('" + $(this).children('.carousel-background').children('img').attr('src') + "')");
+        $(this).children('.carousel-background').remove();
+    });
+
+    $(".carousel").swipe({
+        swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+            if (direction == 'left') $(this).carousel('next');
+            if (direction == 'right') $(this).carousel('prev');
+        },
+        allowPageScroll: "vertical"
+    });
+    /*Intro-Carousel End*/
+    
+});
