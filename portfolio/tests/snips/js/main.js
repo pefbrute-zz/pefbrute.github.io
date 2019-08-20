362e70b7aca2e32dc13df319bac9d357d5f241c1
Fedor Pasynkov <proggraming@mail.ru>
2019-08-20 19:20:27 +0300

    +almost-nothing-new

--- portfolio/tests/snips/js/main.js
+++ portfolio/tests/snips/js/main.js
@@ -318,7 +318,8 @@ $(document).ready(function () {
             }
         }
     }
-
+    
+    //Closure Example
     const counterConstructor = () => {
         let count = 0; //You can't access left variable — it's private variable.
         return () => {
