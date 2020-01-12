c182e776a72e3dc3a8395e9a424db5a428bd94cd
Fedor Pasynkov <proggraming@mail.ru>
2020-01-12 22:44:35 +0300

    +tests

--- portfolio/exercises/Tests/js/main.js
+++ portfolio/exercises/Tests/js/main.js
@@ -1,25 +1,25 @@
-$(document).ready(function () {
+// $(document).ready(function () {
     /*AOS START*/
-    AOS.init({
-        // Global settings:
-        disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-        initClassName: 'aos-init', // class applied after initialization
-        animatedClassName: 'aos-animate', // class applied on animation
-        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    // AOS.init({
+    //     // Global settings:
+    //     disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    //     startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    //     initClassName: 'aos-init', // class applied after initialization
+    //     animatedClassName: 'aos-animate', // class applied on animation
+    //     useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    //     disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    //     debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    //     throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
-        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-        offset: 120, // offset (in px) from the original trigger point
-        delay: 0, // values from 0 to 3000, with step 50ms
-        duration: 400, // values from 0 to 3000, with step 50ms
-        easing: 'ease', // default easing for AOS animations
-        once: false, // whether animation should happen only once - while scrolling down
-        mirror: false, // whether elements should animate out while scrolling past them
-        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-    });
+    //     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    //     offset: 120, // offset (in px) from the original trigger point
+    //     delay: 0, // values from 0 to 3000, with step 50ms
+    //     duration: 400, // values from 0 to 3000, with step 50ms
+    //     easing: 'ease', // default easing for AOS animations
+    //     once: false, // whether animation should happen only once - while scrolling down
+    //     mirror: false, // whether elements should animate out while scrolling past them
+    //     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+    // });
     /*AOS END*/
 
 
@@ -123,6 +123,8 @@ $(document).ready(function () {
         var editor = document.getElementById("editor");
         console.log(editor.getElementsByTagName("*"));
         console.log(editor.querySelector("*"));
+        console.log("Child nodes: " ,editor.childNodes);
+        console.log("Children: ",editor.children);
         let space = function space() {
             editor.appendChild(document.createTextNode(" "));
         }
@@ -184,7 +186,9 @@ $(document).ready(function () {
             var fakeFocusText = focusNode.textContent;
             console.log('Focus is a clear text');
         } else {
-            var fakeFocusText = fakeFocusNode.textContent.trim();
+            // var fakeFocusText = fakeFocusNode.textContent.trim();
+            var fakeFocusText = fakeFocusNode.textContent;
+            console.log("Focus isn't a clear text");
         }
         console.log(fakeAnchorNode);
         console.log(fakeFocusNode);
@@ -301,4 +305,4 @@ $(document).ready(function () {
         // editor.appendChild(lastB);
     }
 
-})
+// })
