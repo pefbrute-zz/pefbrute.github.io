d1d1358632e8a582503fad0f7c2a927629090894
Fedor Pasynkov <proggraming@mail.ru>
2020-02-21 22:19:02 +0300

    +nothing

--- portfolio/exercises/Tests/js/main.js
+++ portfolio/exercises/Tests/js/main.js
@@ -197,6 +197,7 @@ document.getElementById("makeB").onclick = function () {
     //
     //
     //
+    //
     let anchorParentNode = anchorNode.parentNode;
     let focusParentNode = focusNode.parentNode;
     let fakeAnchorNode = anchorParentNode.cloneNode(true);
